6144044dce52ba00d64d443f8024929c566268c7
kevin <kevincaiza131@gmail.com>
2025-02-27 20:01:41 -0500

    git action

--- local/infra2/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region                   = "us-east-1" # Reemplaza con tu región
-  profile                  = "tfm"
-  shared_credentials_files = "~/.aws/credentials"
-  shared_config_files      = "~/.aws/config"
-}
--- local/infra2/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "~> 4.0"
-  required_providers {
-    aws = {
-        source = "registry.terraform.io/hashicorp/aws"
-
-    }
-  }
-}
